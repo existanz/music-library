c2a02d6fb89507928fb7bc77190ca35d986d8765
EXisTAnZ <ozim_ssspam@mail.ru>
2024-10-29 15:57:14 +0300

    feat: configure logger

--- internal/customErrors/customErrors.go
+++ internal/customErrors/customErrors.go
@@ -2,4 +2,8 @@ package customErrors
 
 import "errors"
 
-var ErrNotFound = errors.New("song not found")
+var (
+	ErrNotFound    = errors.New("song not found")
+	ErrInvalidData = errors.New("invalid data")
+	ErrISE         = errors.New("internal server error")
+)
